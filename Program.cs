21e9eabde1bdce7692c1c9e99cea5df6bdfbdfc1
velican <120579492+ashborn211@users.noreply.github.com>
2024-05-17 14:27:08 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP2_Spotivy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# OOP2-Spotivy
+ 
