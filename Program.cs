f9a23a4c7405aedfd7312faa68e5c1149a395869
velican <120579492+ashborn211@users.noreply.github.com>
2024-06-14 14:38:35 +0200

    gewoon alle class names

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace OOP2_Spotivy
+﻿using System;
+
+namespace OOP2_Spotivy
 {
     internal class Program
     {
@@ -9,21 +11,31 @@
             while (!exit)
             {
                 Console.WriteLine("Menu:");
-                Console.WriteLine("1. Option 1");
-                Console.WriteLine("2. Option 2");
-                Console.WriteLine("3. Close Program");
+                Console.WriteLine("1. View all songs");
+                Console.WriteLine("2. View all albums");
+                Console.WriteLine("3. View all artists");
+                Console.WriteLine("4. Create a playlist");
+                Console.WriteLine("5. View your playlists");
+                Console.WriteLine("6. View friends");
+                Console.WriteLine("7. Close Program");
                 Console.Write("Choose an option: ");
 
                 string choice = Console.ReadLine();
                 switch (choice)
                 {
                     case "1":
-                        Console.WriteLine("You chose Option 1");
                         break;
                     case "2":
-                        Console.WriteLine("You chose Option 2");
                         break;
                     case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    case "6":
+                        break;
+                    case "7":
                         Console.WriteLine("Closing program...");
                         exit = true;
                         break;
@@ -31,7 +43,7 @@
                         Console.WriteLine("Invalid option, please try again.");
                         break;
                 }
-                Console.WriteLine(); // Just to add a blank line for readability
+                Console.WriteLine();
             }
         }
     }
